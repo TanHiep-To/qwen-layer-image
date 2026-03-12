9e9da669d75e3600abfd655c5e5890f55d8fba8b
TanHiep-To <tanhiep2012003@gmail.com>
2026-03-12 23:38:02 +0700

    add config bypass proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "http://101.47.76.33/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
